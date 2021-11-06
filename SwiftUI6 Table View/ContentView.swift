441a58fb1ed41ce75c6cfebc78e81c239580e5d5
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-06 11:22:49 +0800

    Initial Commit

---
+++ SwiftUI6 Table View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI6 Table View
+//
+//  Created by Prakhar Trivedi on 6/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI6 Table View/SwiftUI6_Table_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI6_Table_ViewApp.swift
+//  SwiftUI6 Table View
+//
+//  Created by Prakhar Trivedi on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI6_Table_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
